f947bac8b04e35623573822583cd1c9560f97173
Charles Feval <chfeval@microsoft.com>
2019-03-25 18:09:58 -0700

    Making the goal 2d

--- iterations.js
+++ iterations.js
@@ -7,7 +7,7 @@ function Ui() {
 
     loadUi = () => {
         canvas = document.getElementById("canvas");
-        xUsableSpace = canvas.width - 2 * margin;
+        xUsableSpace = canvas.width / 2 - 2 * margin;
         yUsableSpace = canvas.height - 2 * margin;
         canvasContext = canvas.getContext("2d");
         // Todo
@@ -26,7 +26,6 @@ function Ui() {
         canvasContext.arc(center.x, center.y, radius, 0, 2* Math.PI);
         canvasContext.fillStyle = fill;
         canvasContext.fill();
-        //canvasContext.endPath();
     }
 
     writeText = (bottomRight, text, color = defaultColor, font = defaultFont) => {
@@ -40,15 +39,14 @@ function Ui() {
     }
 
     initCanvas = () => {
-        previousProgress = {x: 0, y: 0};
         clearCanvas();
         drawCircle({x: margin, y: canvas.height / 2}, 5, "red");
         writeText({x: margin, y: canvas.height / 2 - 10}, "Start", "red");
-        drawLine(
-            { x: canvas.width - margin, y: margin},
-            { x: canvas.width - margin, y: canvas.height - margin },
+        drawCircle(
+            { x: canvas.width / 2 - margin - 10, y: canvas.height / 2 - margin },
+            20,
             "green");
-        writeText({x: canvas.width - margin - 40, y: canvas.height / 2 - 10},
+        writeText({x: canvas.width / 2 - margin - 27, y: canvas.height / 2 - 35},
             "Finish", 
             "green");
     }
@@ -68,10 +66,6 @@ function Ui() {
     }
 
     loadUi();
-    // .initCanvas();
-    // .drawProgress({x: 0, y: 0}, {x: 5, y: -5}, {x: 10, y: 10}, "red");
-    // .drawProgress({x: 5, y: -5}, {x: 10, y: 0}, {x: 10, y: 10}, "green");
-
 
     return {
         init: initCanvas,
@@ -82,34 +76,54 @@ function Ui() {
 
 function textUpdater(element) {
     return (state) => {
-        element.innerHTML = `Color: ${state.color}. Frequency: ${state.frequency}. Iteration: ${state.iteration}. Progress: ${Math.round(state.currentPos.x)}. Days: ${state.day}`;
+        element.innerHTML = `Color: ${state.color}. Frequency: ${state.frequency}. Iteration: ${state.iteration}. DistanceToFinish: ${Math.round(state.distanceToFinish)}. Days: ${state.day}.`;
     }
 }
 
-function loadTick(ui, algorithm, frequency, color, callback) {
-    const goal = 200;
-    const maxY = goal / ui.screenRatio;
+function updatePosition(cycleLength, generateDirectionForNextIteration = () => gaussianRand()* Math.PI) {
+    return (pos, goal) => {
+        const directionToGoal = Math.atan2(goal.y - pos.y, goal.x - pos.x);
+        const directionForNextIteration = generateDirectionForNextIteration() + directionToGoal;
+        return {
+                x: pos.x + Math.cos(directionForNextIteration) * cycleLength,
+                y: pos.y + Math.sin(directionForNextIteration) * cycleLength
+            };
+        };
+}
+
+function distance(pt1, pt2) {
+    return Math.sqrt(Math.pow(pt1.x - pt2.x, 2) + Math.pow(pt1.y - pt2.y, 2));
+}
+
+function loadTick(ui, directionGenerator, cycleLengthInDays, color, callback) {
+    const goal = { x: 1000, y: 0 };
+    const goalSize = 35;
+    const maxY = goal.x / ui.screenRatio;
     const state = {
         currentPos: {x: 0, y: 0},
-        frequency,
+        distanceToFinish: 0,
+        frequency: cycleLengthInDays,
         color,
         prevPos: {x: 0, y: 0},
         iteration: 0,
         day: 0,
         goal: goal,
-        maximums: { x: goal, y: maxY }
+        maximums: { x: goal.x, y: maxY }
     }
+
+    const positionUpdater = updatePosition(cycleLengthInDays, directionGenerator);
 
     const tick = (day) => {
         state.day = day;
-        if (day % frequency === 0) {
+        state.distanceToFinish = distance(state.currentPos, goal);
+        if (day % cycleLengthInDays === 0) {
             state.iteration++;
             state.prevPos = state.currentPos;
-            state.currentPos = algorithm(state.currentPos);
+            state.currentPos = positionUpdater(state.currentPos, goal);
             ui.drawProgress(state.prevPos, state.currentPos, state.maximums, color);
             callback(state);
         }
-        return state.currentPos.x < state.goal;
+        return state.distanceToFinish > goalSize;
     }
     return tick;
 }
@@ -121,13 +135,6 @@ function loop(ticks, periodMs, day = 0) {
     }
 }
 
-function linearAlgorithm(currentPos) {
-    const newPos = {
-        x: currentPos.x + 3,
-        y: currentPos.y + Math.random() * 10 - 5
-    };
-    return newPos;
-}
 
 // Box-muller transform from https://jsfiddle.net/ssell/qzzvruc4/
 function gaussianRand() {
@@ -138,33 +145,17 @@ function gaussianRand() {
     if (this.generate) {
         let x1 = 0.0, x2 = 0.0, w  = 0.0;
         do {
-            // Math.random() gives value on range [0, 1) but
-            // the Polar Form expects [-1, 1].
             x1 = (2.0 * Math.random()) - 1.0;
             x2 = (2.0 * Math.random()) - 1.0;
             w  = (x1 * x1) + (x2 * x2);
         } while(w >= 1.0);
-
         w = Math.sqrt((-2.0 * Math.log(w)) / w);
-
         this.value0 = x1 * w;
         this.value1 = x2 * w;
-
         result = this.value0;
     } else {
         result = this.value1;
     }
-
     this.generate = !this.generate
     return (result / 3.6);
-}
-
-function gaussian(cycleLength, random = () => gaussianRand()* Math.PI) {
-    return (pos) => {
-        const randomAngle = random();
-        return {
-                x: pos.x + Math.cos(randomAngle) * cycleLength,
-                y: pos.y + Math.sin(randomAngle) * cycleLength
-            };
-        };
 }
